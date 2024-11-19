ea6c047c1b3f7c3dde2b53620fe74cfca2275551
Kalcha <diego.rbarrionuevo@gmail.com>
2024-11-18 22:34:13 -0300

    2 commit

--- js/08.js
+++ js/08.js
@@ -4,4 +4,5 @@ const nombre = "Androide Kalchaqui";
 const apellido = "Soto";
 const modelo = "A75";
 
-console.log("Hola, su nombre es: " + nombre +" " + modelo);
+//console.log("Hola, su nombre es: " + nombre +" " + modelo);
+console.log(`Hola, su nombre es: ${nombre} ${apellido} ${modelo}`);
---
+++ js/09.js
@@ -0,0 +1,5 @@
+//Array o Arreglos
+
+const tecnologias = [10, 30, 40]
+
+console.table(tecnologias)
