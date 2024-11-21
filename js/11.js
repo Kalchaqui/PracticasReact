9da07e96a7cf11daa762937ee79d6e97fda2d8ea
Kalcha <diego.rbarrionuevo@gmail.com>
2024-11-20 23:09:11 -0300

    4to commit

---
+++ js/11.js
@@ -0,0 +1,6 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python", "PHP", "Java"]
+
+const react = tecnologias[3]
+
+//destructuring array= dependiendo de la posición de la variable,
+// del arreglo del cual las estoy aplicando
