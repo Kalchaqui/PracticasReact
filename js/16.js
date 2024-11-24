51deb6a1a09e30e60846a1f6ec4aab21b0330a45
Kalcha <diego.rbarrionuevo@gmail.com>
2024-11-24 19:02:18 -0300

    Subiendo todos los archivos correctamente

---
+++ js/16.js
@@ -0,0 +1,15 @@
+//FUNCIONES QUE RETORNAN VALORES
+    //const sumar = (numero1 = 0, numero2 = 0) =>console.log(numero1 + numero2)
+
+//const sumar = function (numero1 = 0, numero2 = 0) {
+//    return numero1 + numero2
+//}
+
+const sumar = (numero1 = 0, numero2 = 0) => numero1 + numero2
+
+
+const resultado = sumar(10, 20)   
+
+
+
+console.log(resultado) 
---
+++ js/17.js
@@ -0,0 +1,15 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python", "PHP", "Java"]
+const numeros = [10, 20, 30]
+
+//Filter
+
+//const nuevoArray = tecnologias.filter((tech) => tech !== "HTML")
+//const resultado = numeros.filter(numero => numero !== 15)
+
+//const nuevoArray = numeros.filter(function (kalcha) { 
+  //  console.log(kalcha)
+ //})
+
+ //Includes
+ const resultado = tecnologias.includes("CSS")
+ console.log(resultado)
