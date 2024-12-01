7a69c9199bc6d9615217d41f98296e0a2d304cc8
Kalcha <diego.rbarrionuevo@gmail.com>
2024-11-30 21:51:10 -0300

    funciones

---
+++ js/24.js
@@ -0,0 +1,22 @@
+//import{sumar} from "./funciones.js"
+
+//const resultado = sumar(29, 10)
+
+//console.log(resultado)
+
+import {sumar, restar, multiplicar, division} from "./funciones.js"
+
+const resultado1 = sumar(29, 10)
+const resultado2 = restar(29, 10)
+const resultado3 = multiplicar(29, 10)
+const resultado4 = division(29, 10)
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+
+
+
+
+
---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+//##(type="module")habilita la funsion de importar <script src="js/24.js" type="module"></script>  
+
+//export function sumar(n1, n2){
+  //  return n1 + n2;
+//}
+
+//export function restar(n1, n2){
+  //  return n1 - n2;
+//}
+
+export const sumar = (n1, n2) => n1 + n2
+
+export const restar = (n1, n2) => n1 - n2
+
+export const multiplicar = (n1, n2) => n1 * n2
+
+export const division = (n1, n2) => n1 / n2
+
