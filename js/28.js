66cc077ddd93b1251f9d9deb1d53aa202e2a759d
kalchaqui <diego.rbarrionuevo@gmail.com>
2024-12-09 15:24:55 -0300

    29js

---
+++ js/28.js
@@ -0,0 +1,7 @@
+//Selectores
+
+//¿(DOM) es una interfaz de programación que permite a JavaScript acceder 
+//y modificar los elementos HTML de una página web. 
+const pingo = document.querySelector('.fo')
+
+console.log(pingo)
---
+++ js/29.js
@@ -0,0 +1,12 @@
+
+//Manipular elementos HTML con JS
+const heading = document.querySelector(".heading")
+const enlaces = document.querySelectorAll(".navegacion a")
+
+heading.textContent = "Nuevo Heading..."
+
+const inputNombre = document.querySelector("#nombre")
+inputNombre.value = "Nuevo valor"
+const inputPassword = document.querySelector("#password")
+inputPassword.value = "Nuevo valor"
+
