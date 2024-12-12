ddbfec890b0e950e5630b230d610112e3a30cb90
kalchaqui <diego.rbarrionuevo@gmail.com>
2024-12-12 11:14:33 -0300

    32js

--- js/32.js
+++ js/32.js
@@ -6,15 +6,20 @@ const nombre = document.querySelector("#nombre").value
 const password = document.querySelector("#password").value
 
 const alerta = document.createElement("DIV")
+//alerta.textContent = 'Contenido de Alerta'
+//alerta.classList.add('clase-1','clase-2','clase-3')
+//console.log(alerta)
 alerta.classList.add('alerta', 'text-white', 'uppercase', 
     'text-sm', 'text-center','p-2', 'font-black')
-
-if(nombre === "" || password === ""){
-    alerta.textContent= "Todos los campos son obligatorios"
-    alerta.classList.add('bg-red-300')
+if(nombre === '' || password === ''){
+    //console.log('todos los campos son obligatorios')
+    alerta.textContent = 'Todos los campos son obligatorios'
+    alerta.classList.add('bg-red-500')
 }else {
+    //console.log('todo bien')
+
     alerta.textContent="todo bien!!! iniciar sesion"
-    alerta.classList.add('bg-black-300')
+    alerta.classList.add('bg-blue-500')
 }
 formulario.appendChild(alerta)
 //formulario appendChild es un hijo de
@@ -22,5 +27,6 @@ setTimeout(()=> {
 
 }, 3000);
 
+//console.log(alerta)
 
 })
